aca663d340d63312f0c3437a0f1e8e8ef09cb72f
Berislav Lopac <berislav@prix.ai>
2016-11-23 16:53:40 +0000

    cleanup and use prix.ai domain

--- prix_collection.js
+++ prix_collection.js
@@ -1,32 +1,32 @@
-    var prix = {
-        send_event: function(data) {
-            this._prepare(data).forEach(function(element){
-                var r = new XMLHttpRequest();
-                r.onreadystatechange = function () {
-                    if (r.responseText) {
-                        console.log(r.responseText);
-                    }
-                };
-                r.open("POST", "http://localhost:8080/events", true);
-                r.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                r.send(JSON.stringify(element));
-            });
-        },
-        _prepare: function(data) {
-            if (data.constructor !== Array) {
-                data = [data];
-            }
-            return data.map(function(element){
-                return {
-                    type: element.type,
-                    occurred: new Date().toISOString(),
-                    data: {
-                        merchant: element.merchant,
-                        product: element.product,
-                        channel: element.channel,
-                        price: element.price,
-                    }
+var prix = {
+    send_event: function(data) {
+        this._prepare(data).forEach(function(element){
+            var r = new XMLHttpRequest();
+            r.onreadystatechange = function () {
+                if (r.responseText) {
+                    console.log(r.responseText);
                 }
-            });
+            };
+            r.open("POST", "http://collection.api.prix.ai:8080/events", true);
+            r.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            r.send(JSON.stringify(element));
+        });
+    },
+    _prepare: function(data) {
+        if (data.constructor !== Array) {
+            data = [data];
         }
+        return data.map(function(element){
+            return {
+                type: element.type,
+                occurred: new Date().toISOString(),
+                data: {
+                    merchant: element.merchant,
+                    product: element.product,
+                    channel: element.channel,
+                    price: element.price,
+                }
+            }
+        });
     }
+}
